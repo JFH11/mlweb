b9f7dac1ef37e5f7aad863d253b74385b4a58ead
Junziroo <raditya.arjun64@smk.belajar.id>
2025-06-04 12:10:22 +0700

    v

--- js/index.js
+++ js/index.js
@@ -13,20 +13,26 @@ function showLoader(event) {
 
 const keranjangBtn = document.querySelector('.btn-keranjang');
 const keranjang = document.querySelector('.container-keranjang');
+const maink = document.querySelector('main');
 let munculKeranjang = false;
 
 keranjangBtn.addEventListener("click", () => {
   munculKeranjang = !munculKeranjang;
-  keranjang.style.visibility = munculKeranjang ? 'visible' : 'hidden';
+  keranjang.style.top = munculKeranjang ? '120px' : '-400px';
   keranjang.style.transform = munculKeranjang ? 'scale(1)' : 'scale(0)';
+  maink.style.filter = munculKeranjang ? 'blur(5px)' : 'none';
+  maink.style.pointerEvents = munculKeranjang ? 'none' : 'auto';
 });
 
 const menu = document.querySelector('.menu');
 const menuContainer = document.querySelector('.menu-container');
+const mainz = document.querySelector('main');
 let menuMuncul = false;
 
 menu.addEventListener('click', () => {
   menuMuncul = !menuMuncul;
   menuContainer.style.bottom = menuMuncul ? '0' : '-300px';
+  mainz.style.filter = menuMuncul ? 'blur(5px)' : 'none';
+  mainz.style.pointerEvents = menuMuncul ? 'none' : 'auto';
 });
 
